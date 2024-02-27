de9ac10a97fdc01f25e9ccf4d02b755c0aab94ef
at <98226985+Croixed@users.noreply.github.com>
2024-02-27 14:57:11 -0800

    refactored home page blog card

---
+++ app/components/HomeCard.tsx
@@ -0,0 +1,37 @@
+import { Card, CardContent } from "@/components/ui/card";
+import Image from "next/image";
+import { urlFor } from "../lib/sanity";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { blogCard } from "../lib/interface";
+
+export default function HomeCard({ post }: { post: blogCard }) {
+  return (
+    <Card className="flex flex-col border-none bg-background shadow-none md:flex-row">
+      <div className="relative h-60 w-screen flex-shrink-0 md:h-60 md:w-60">
+        <Image
+          src={urlFor(post.titleImage).url()}
+          alt="image"
+          fill={true}
+          className="object-cover md:h-[200px]"
+        />
+      </div>
+      <CardContent className="m-3 flex-grow p-0">
+        <Link href={`/blog/${post.currentSlug}`} className="">
+          <div className="flex h-full flex-col justify-between">
+            <h3 className="line-clamp-2 text-3xl font-bold">{post.title}</h3>
+            <p className="text-md my-4 line-clamp-3 font-serif text-gray-600 dark:text-gray-400 md:my-0">
+              {post.smallDescription}
+            </p>
+            <Button
+              variant="link"
+              className="m-0 h-6 w-min p-0 text-lg font-bold uppercase"
+            >
+              Read More
+            </Button>
+          </div>
+        </Link>
+      </CardContent>
+    </Card>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,8 @@
-import { Card, CardContent } from "@/components/ui/card";
 import { blogCard } from "./lib/interface";
-import { client, urlFor } from "./lib/sanity";
-import Image from "next/image";
-import { Button } from "@/components/ui/button";
+import { client } from "./lib/sanity";
 import { Separator } from "@/components/ui/separator";
-import Link from "next/link";
 import Featured from "./components/Featured";
+import HomeCard from "./components/HomeCard";
 
 export const revalidate = 40;
 
@@ -24,54 +21,16 @@ async function getData() {
 
 export default async function Home() {
   const data: blogCard[] = await getData();
-
-  console.log(data);
+  // console.log(data);
   return (
     <>
       <p className="pl-4 pt-4 font-bold uppercase md:pl-0">Featured</p>
       <Featured data={data} />
       <p className="pl-4 pt-4 font-bold uppercase md:pl-0">Latest</p>
       <div className="my-5 grid grid-cols-1 gap-1 md:gap-5">
-        {/* should probably use a better key than an iterator? */}
         {data.map((post, idx) => (
           <>
-            <Card
-              className="flex flex-col border-none bg-background shadow-none md:flex-row"
-              key={idx}
-            >
-              <div className="relative h-60 w-screen flex-shrink-0 md:h-60 md:w-60">
-                <Image
-                  src={urlFor(post.titleImage).url()}
-                  alt="image"
-                  // width={600}
-                  // height={600}
-                  fill={true}
-                  className="object-cover md:h-[200px]"
-                />
-              </div>
-              <CardContent className="m-3 flex-grow p-0">
-                <div className="flex h-full flex-col justify-between">
-                  <h3 className="line-clamp-2 text-3xl font-bold">
-                    {post.title}
-                  </h3>
-                  <p className="text-md my-4 line-clamp-3 font-serif text-gray-600 dark:text-gray-400 md:my-0">
-                    {post.smallDescription}
-                  </p>
-                  <Button
-                    variant="link"
-                    asChild
-                    className="m-0 h-6 w-min p-0 text-lg font-bold uppercase"
-                  >
-                    <Link href={`/blog/${post.currentSlug}`} className="">
-                      Read More
-                    </Link>
-                  </Button>
-                </div>
-              </CardContent>
-            </Card>
-            {/* <div className="">
-
-            </div> */}
+            <HomeCard post={post} key={post.currentSlug} />
             {idx !== data.length - 1 ? (
               <Separator className="hidden md:block" />
             ) : null}
